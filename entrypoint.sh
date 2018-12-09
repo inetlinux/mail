432276dfaa7443ae03f38aefdd56cc5ffdfdb298
Jing Li <lijing@example.com>
2018-12-09 22:23:54 +0800

    modify dovecot config, for auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,18 @@ if [ $# -gt 1 -a "$1" = "/usr/bin/supervisord" ]; then
     ln -s /etc/pki/tls/certs/dovecot.pem /etc/dovecot/dovecot.pem
     ln -s /etc/pki/tls/private/dovecot.pem /etc/dovecot/private/dovecot.pem
 
+    postconf -e 'inet_interfaces = all'
     postconf -e mydomain=$mydomain
     postconf -e myhostname=mail.$mydomain
-    postconf -e 'inet_interfaces = all'
     postconf -e "mydestination = localhost, localhost.$mydomain, $myhostname, $mydomain"
-    postconf -e 'smtpd_sasl_type = dovecot'
-    postconf -e 'smtpd_sasl_auth_enable = yes'
-    postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination'
-    postconf -e 'smtpd_sasl_path = private/auth'
-    postconf -e message_size_limit=52428800
+    postconf -e 'home_mailbox = mail/'
+    postconf -e 'mailbox_command = /usr/libexec/dovecot/dovecot-lda -f "$SENDER" -a "$RECIPIENT"'
+    postconf -e 'message_size_limit=52428800'
+
+   # postconf -e 'smtpd_sasl_type = dovecot'
+   # postconf -e 'smtpd_sasl_auth_enable = yes'
+   # postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination'
+   # postconf -e 'smtpd_sasl_path = private/auth'
 fi
 
 exec "$@"
